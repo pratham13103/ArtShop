9fb8ac1bd255b112409abe65c6b7d2aae7fc4286
pratham13103 <167630988+pratham13103@users.noreply.github.com>
2025-03-11 16:44:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ArtShop/' // Set this to your GitHub repo name
+  base: '/ArtShop/', // Make sure this matches your GitHub repo name exactly
 })
